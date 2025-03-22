c4054fb382e1a217fc250d1393ab3112e91c27f4
alex99091 <bkk99091@gmail.com>
2025-03-22 10:22:42 +0900

    FEAT: updated changes

--- analyzer/src/main/java/com/random/analyzer/model/DrawResult.java
+++ analyzer/src/main/java/com/random/analyzer/model/DrawResult.java
@@ -22,7 +22,8 @@ public class DrawResult {
     // ✅ 기본 생성자 추가 (JPA에서 필수)
     public DrawResult() {}
 
-    public DrawResult(int number1, int number2, int number3, int number4, int number5, int number6) {
+    public DrawResult(int idx, int number1, int number2, int number3, int number4, int number5, int number6) {
+        this.idx = idx;
         this.number1 = number1;
         this.number2 = number2;
         this.number3 = number3;
